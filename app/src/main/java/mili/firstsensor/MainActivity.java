fa80a3eb7cd3e85772ea96d70c2163aeed8de672
Ming Li <mili@mines.edu>
2017-04-20 11:17:30 -0600

    Play Single Tone

--- app/src/main/java/mili/firstsensor/MainActivity.java
+++ app/src/main/java/mili/firstsensor/MainActivity.java
@@ -1,13 +1,27 @@
 package mili.firstsensor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button playSoundButton = (Button) findViewById(R.id.play_sound_button);
+        playSoundButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlaySound.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
